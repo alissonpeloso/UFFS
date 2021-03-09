83846c0aadeebbf320f4085c253d2754bb62764d
Alisson Luan de Lima Peloso <51202705+alissonpeloso@users.noreply.github.com>
2021-03-09 11:22:46 -0300

    Delete Semestre_2 directory

--- Semestre_2/Circuitos_Digitais/Sea_Battle/Quartus II/simulation/qsim/work/@sea_@battle_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Sea_Battle_vlg_vec_tst is
-end Sea_Battle_vlg_vec_tst;
--- Semestre_2/Estrutura_de_Dados/Exercices/Revisão/1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int x, *p, *q;
-    p = &x;
-    q = p;
-    x = 10;
-    printf("\n%d\n", *q);
-    return(0);
-}
