5fb1cca5cfef078f0459ad8fba57bb43ed5511bc
Alisson Peloso <alisson.luan2000@gmail.com>
2022-10-10 14:02:38 -0300

    update

--- Semestre_8/TCC2/Implementation/get-stock.py
+++ Semestre_8/TCC2/Implementation/get-stock.py
@@ -1,13 +1,7 @@
-# Raw Package
 import numpy as np
 import pandas as pd
-
-# Data Source
 import yfinance as yf
-
-# Data viz
 import plotly.graph_objs as go
-
 import datetime
 
 def getStockPrice(stock, period):
