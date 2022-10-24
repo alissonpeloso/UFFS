3b67df04a87ed053459ef2225aa4fc37fe3af1bd
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2022-10-24 15:39:13 -0300

    update

--- Semestre_8/Programacao_competitiva/1622-creating_strings.cpp
+++ Semestre_8/Programacao_competitiva/1622-creating_strings.cpp
@@ -3,14 +3,27 @@ using namespace std;
  
 typedef long long ll;
 
+string a;
+string permutation;
+set<string> strings;
+int n;
+vector<bool> chosen; 
+
 void search() {
     if (permutation.size() == n) {
-        // process permutation
+        bool test = true;
+        for(int i = 0; i < chosen.size(); i++) {
+            test = test && chosen[i];
+        }
+        if(test) {
+            strings.insert(permutation);
+        }
+        return;
     } else {
         for (int i = 0; i < n; i++) {
             if (chosen[i]) continue;
             chosen[i] = true;
-            permutation.push_back(i);
+            permutation.push_back(a[i]);
             search();
             chosen[i] = false;
             permutation.pop_back();
@@ -20,8 +33,17 @@ void search() {
  
 int main()
 {
-    string a;
     cin >> a;
+    n = a.size();
+    for(int i = 0; i < n; i++) {
+        chosen.push_back(false);
+    }
 
+    search();
 
+    cout << strings.size() << endl;
+
+    for (auto it = strings.begin(); it != strings.end(); ++it) {
+        cout << *it << endl;
+    }
 }
