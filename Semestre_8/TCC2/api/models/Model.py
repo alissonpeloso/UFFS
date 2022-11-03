36595b953763206bb7804d1963cdfa5bcf06f262
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2022-11-03 07:47:37 -0300

    update

--- Semestre_8/TCC2/api/models/Model.py
+++ Semestre_8/TCC2/api/models/Model.py
@@ -9,10 +9,10 @@ import tensorflow as tf
 from Stock import Stock
 import json
 
-weightsFolder = 'weights/';
+weightsFolder = 'weights/'
 
 # config object example
-# config = 
+# config =
 # {
 # 	X_columns : ['Fechamento', 'Volume Quantidade'],
 # 	y_columns : ['Fechamento'],
@@ -44,30 +44,42 @@ weightsFolder = 'weights/';
 #   }
 # }
 
+
 class Model:
-    def __init__(self, config = {}, timeframe = 2, periods = 7200):
+    def __init__(self, config={}, timeframe=2, periods=7200):
         self.config = config
         if (config.get('stock') != None):
             stock = Stock(timeframe, periods)
         else:
             stock = Stock(2, 7200)
-        dataset = stock.getDF()
+        stock.getDF()
 
-        self.dataset = dataset.replace({',': '.'}, regex=True)
-        self.X_columns = config['X_columns'] if config.get('X_columns') != None else ['Fechamento', 'Volume Quantidade']
-        self.y_columns = config['y_columns'] if config.get('y_columns') != None else ['Fechamento']
-        self.n_steps = config['n_steps'] if config.get('n_steps') != None else 144
-        self.m_predictions = config['m_predictions'] if config.get('m_predictions') != None else 13
-        self.test_size = config['test_size'] if config.get('test_size') != None else 0.0
-        self.X = None 
+        # Adding MA
+        if config.get('MA') != None:
+            for i in config.get('MA'):
+                stock.MA(i)
+
+        self.dataset = stock.df
+        self.X_columns = config['X_columns'] if config.get('X_columns') != None else [
+            'Fechamento', 'Volume Quantidade']
+        self.y_columns = config['y_columns'] if config.get(
+            'y_columns') != None else ['Fechamento']
+        self.n_steps = config['n_steps'] if config.get(
+            'n_steps') != None else 144
+        self.m_predictions = config['m_predictions'] if config.get(
+            'm_predictions') != None else 13
+        self.test_size = config['test_size'] if config.get(
+            'test_size') != None else 0.0
+        self.X = None
         self.y = None
         self.scaler_X = None
         self.scaler_y = None
+
         print(self.dataset[-10:-1])
         self.prepare_dataset()
 
     def prepare_dataset(self):
-        ### Scaling X and y parameters
+        # Scaling X and y parameters
         X_unscaled = self.dataset[self.X_columns].iloc[:, :].values
         y_unscaled = self.dataset[self.y_columns].iloc[:, :].values
 
@@ -83,27 +95,29 @@ class Model:
             end_ix = i + self.n_steps
             end_iy = end_ix + self.m_predictions
             # check if we are beyond the sequence
-            if end_ix > len(X_scaled)-1 or end_iy > len(X_scaled)-1 :
+            if end_ix > len(X_scaled)-1 or end_iy > len(X_scaled)-1:
                 break
             # gather input and output parts of the pattern
-            seq_x, seq_y = X_scaled[i:end_ix], y_scaled[end_ix:end_ix+self.m_predictions] 
+            seq_x, seq_y = X_scaled[i:end_ix], y_scaled[end_ix:end_ix +
+                                                        self.m_predictions]
 
             X.append(seq_x)
             y.append(seq_y)
 
         self.X = np.array(X)
         self.y = np.array(y)
-        
+
         size = len(self.X)
         self.X_train = np.array(X[:int(size - (size*self.test_size))])
-        self.X_test = np.array(X[int(size - (size*self.test_size)) : -1])
+        self.X_test = np.array(X[int(size - (size*self.test_size)): -1])
         self.y_train = np.array(y[:int(size - (size*self.test_size))])
-        self.y_test = np.array(y[int(size - (size*self.test_size)) : -1])     
+        self.y_test = np.array(y[int(size - (size*self.test_size)): -1])
 
-    def fit(self, model_name = 'default.h5'):
+    def fit(self, model_name='default.h5'):
         X_train, y_train = self.X_train, self.y_train
 
-        layers = (self.config['layers'] if self.config.get('layers') != None else [])
+        layers = (self.config['layers'] if self.config.get(
+            'layers') != None else [])
 
         if (len(layers) == 0):
             layers = [
@@ -130,36 +144,43 @@ class Model:
 
         for index, layer in enumerate(layers):
             if (index == 0):
-                regressor.add(LSTM(units=layer['units'], return_sequences=True, input_shape=(X_train.shape[1], len(self.X_columns))))
+                regressor.add(LSTM(units=layer['units'], return_sequences=True, input_shape=(
+                    X_train.shape[1], len(self.X_columns))))
             elif (index == len(layers) - 1):
                 regressor.add(LSTM(units=layer['units']))
             else:
-                regressor.add(LSTM(units=layer['units'], return_sequences=True))
+                regressor.add(
+                    LSTM(units=layer['units'], return_sequences=True))
             regressor.add(Dropout(layer['dropout']))
 
         # Output layer
-        regressor.add(Dense(units=self.m_predictions, activation='sigmoid')) ## 8 ou 13
+        regressor.add(Dense(units=self.m_predictions,
+                      activation='sigmoid'))  # 8 ou 13
 
         regressor.add(Reshape((self.m_predictions, 1)))
 
         # Building the network
         regressor.compile(optimizer='rmsprop', loss='mean_squared_error',
-                            metrics=['mean_absolute_error'])
+                          metrics=['mean_absolute_error'])
 
         # Funções de Callback
-        es = EarlyStopping(monitor='loss', min_delta=1e-10, patience=10, verbose=1)
-        rlr = ReduceLROnPlateau(monitor='loss', factor=0.2, patience=5, verbose=1)
+        es = EarlyStopping(monitor='loss', min_delta=1e-10,
+                           patience=10, verbose=1)
+        rlr = ReduceLROnPlateau(
+            monitor='loss', factor=0.2, patience=5, verbose=1)
         mcp = ModelCheckpoint(filepath='pesos.h5', monitor='loss',
-                                save_best_only=True, verbose=1)
-        regressor.fit(X_train, y_train, epochs=100, batch_size=32 ,callbacks=[es, rlr, mcp])
+                              save_best_only=True, verbose=1)
+        regressor.fit(X_train, y_train, epochs=100,
+                      batch_size=32, callbacks=[es, rlr, mcp])
 
         regressor.save(weightsFolder+model_name)
-    
-    def test(self, model_name = 'default.h5'):
+
+    def test(self, model_name='default.h5'):
         regressor = load_model(weightsFolder+model_name)
         y_hat = regressor.predict(self.X_test)
         y_hat = np.reshape(y_hat, (y_hat.shape[0], y_hat.shape[1]))
-        y_test = np.reshape(self.y_test, (self.y_test.shape[0], self.y_test.shape[1]))
+        y_test = np.reshape(
+            self.y_test, (self.y_test.shape[0], self.y_test.shape[1]))
         y_hat = self.scaler_y.inverse_transform(y_hat)
         y_test = self.scaler_y.inverse_transform(y_test)
 
@@ -175,15 +196,16 @@ class Model:
         r2 = r2_score(y_test, y_hat)
         print('R2: %.3f' % r2)
 
-    
-    def predict(self, model_name = 'default.h5'):
+    def predict(self, model_name='default.h5'):
         regressor = load_model(weightsFolder+model_name)
         test = self.X[-1].reshape(1, self.X[-1].shape[0], self.X[-1].shape[1])
         y_hat = regressor.predict(test)
         y_hat = np.reshape(y_hat, (y_hat.shape[0], y_hat.shape[1]))
         return self.scaler_y.inverse_transform(y_hat)
 
+
 with open('config/1.json') as json_file:
     config = json.load(json_file)
     lstm = Model(config, 2, 7200)
-    lstm.test("2min_default.h5")
+    lstm.fit("2min_MA21.h5")
+    lstm.test("2min_MA21.h5")
--- Semestre_8/TCC2/api/models/Stock.py
+++ Semestre_8/TCC2/api/models/Stock.py
@@ -1,11 +1,12 @@
-import yfinance as yf
 import pandas as pd
 
+
 class Stock:
-    def __init__(self, timeframe, period = None):
+    def __init__(self, timeframe, period=None):
         self.period = period
         self.timeframe = int(timeframe)
-    
+        self.df = None
+
     def getDF(self):
         period = self.period
         timeframe = self.timeframe
@@ -15,7 +16,18 @@ class Stock:
 
         if period == None:
             return df
-            
-        df = df[-period:-1]
-        return df
 
+        df = df[-period:-1]
+        self.df = df.replace({',': '.'}, regex=True)
+        self.df.astype({
+            'Abertura': 'float64',
+            'Máxima': 'float64',
+            'Mínima': 'float64',
+            'Fechamento': 'float64',
+            'Volume Quantidade': 'int32'
+        }, copy=False)
+
+    def MA(self, windowSize, columnName):
+        numberSeries = self.df[columnName].squeeze()
+        movingAverages = numberSeries.rolling(windowSize).mean()
+        self.df["MA"+str(windowSize)] = movingAverages
