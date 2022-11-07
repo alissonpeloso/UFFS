3a6dadbe46e1152745c68b1e54c79173c9b65882
Alisson Peloso <alisson.luan2000@gmail.com>
2022-11-07 19:25:54 -0300

    update

--- Semestre_8/Programacao_competitiva/1090-ferris_wheel.cpp
+++ Semestre_8/Programacao_competitiva/1090-ferris_wheel.cpp
@@ -5,5 +5,44 @@ typedef long long ll;
  
 int main()
 {
-    
+    ll n, x, ct = 0, start, end;
+    cin >> n >> x;
+
+    vector<ll> p;
+    for (int i = 0; i < n; i++)
+    {
+        ll a;
+        cin >> a;
+        p.push_back(a);
+    }
+
+    sort(p.rbegin(), p.rend());
+    start = 0;
+    end = n - 1;
+
+    if(start > x) {
+        start = lower_bound(p.begin(), p.end(), x) - p.begin();
+    }
+
+    while (start <= end)
+    {
+        if (p[start] + p[start+1] <= x)
+        {
+            ct++;
+            start+=2;
+        } else {
+            vector<ll>::iterator tt = lower_bound(p.begin()+start, p.begin()+end, x-p[start], greater<ll>());
+
+            if(*tt+p[start] <= x) {
+                p.erase(tt);
+                end--;
+            }
+            
+            ct++;
+            start++;
+        }
+
+    }
+
+    cout << ct << endl;
 }
