7d30119799bc61c03cddce8ce6c92e1db727c650
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2023-01-25 19:26:19 -0300

    update

--- Semestre_8/Programacao_competitiva/graphs/counting_rooms.cpp
+++ Semestre_8/Programacao_competitiva/graphs/counting_rooms.cpp
@@ -3,38 +3,20 @@ using namespace std;
 
 typedef long long ll;
 
-vector <vector<int>> graphs;
-vector <bool> visited;
-int cnt = 0;
-
-void dfs(int s) {
-    if (visited[s]) return;
-    visited[s] = true;
-
-    for (auto u: graphs[s]) {
-        dfs(u);
-    }
-}
-
 int main()
 {
-    int n, m;
+    int n, m, cnt = 0;
     cin >> n >> m;
 
+    queue<pair<int, int>> q;
+
     char matrix[n][m];
-    int roomSpace[n][m];
 
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < m; j++)
         {
             cin >> matrix[i][j];
-            if (matrix[i][j] == '.') {
-                graphs.push_back(vector<int>());
-                visited.push_back(false);
-
-                roomSpace[i][j] = cnt++;
-            }
         }
     }
 
@@ -42,36 +24,49 @@ int main()
     {
         for(int j = 0; j < m; j++)
         {
-            if (matrix[i][j] == '#') continue;
-            if (i > 0 && matrix[i-1][j] == '.') {
-                graphs[roomSpace[i][j]].push_back(roomSpace[i-1][j]);
-            }
-            if (i < n-1 && matrix[i+1][j] == '.') {
-                graphs[roomSpace[i][j]].push_back(roomSpace[i+1][j]);
-            }
-            if (j > 0 && matrix[i][j-1] == '.') {
-                graphs[roomSpace[i][j]].push_back(roomSpace[i][j-1]);
-            }
-            if (j < m-1 && matrix[i][j+1] == '.') {
-                graphs[roomSpace[i][j]].push_back(roomSpace[i][j+1]);
+            if(matrix[i][j] == '.')
+            {
+                cnt++;
+
+                q.push({i, j});
+
+                while(!q.empty())
+                {
+                    pair<int, int> p = q.front();
+                    q.pop();
+
+                    int x = p.first;
+                    int y = p.second;
+
+                    if(matrix[x][y] == '.')
+                    {
+                        matrix[x][y] = '#';
+
+                        if(x + 1 < n)
+                        {
+                            q.push({x + 1, y});
+                        }
+
+                        if(x - 1 >= 0)
+                        {
+                            q.push({x - 1, y});
+                        }
+
+                        if(y + 1 < m)
+                        {
+                            q.push({x, y + 1});
+                        }
+
+                        if(y - 1 >= 0)
+                        {
+                            q.push({x, y - 1});
+                        }
+                    }
+                }
+                
             }
         }
     }
 
-    if(visited.size() == 0) {
-        cout << 0 << endl;
-        return 0;
-    }
-
-    int components = 0;
-    while(true) {
-        int i = find(visited.begin(), visited.end(), false) - visited.begin();
-        if (i == visited.size()) break;
-        dfs(i);
-        components++;
-    }
-
-    cout << components << endl;
-
-    return 0;
+    cout << cnt << endl;
 }
